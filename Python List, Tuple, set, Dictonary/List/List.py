06efb5fe27b8b33185cc1a075981bbdcab514412
Nandhakumar <Nandhaselvi45@gmail.com>
2026-08-05 21:17:53 +0530

    Class and object

---
+++ Python List, Tuple, set, Dictonary/List/List.py
@@ -0,0 +1,45 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+numbers = [10, 20, 30, 40, 50]
+print(numbers[0], [4], [2])
+
+fruits = ["apple", "banana", "cherry"]
+fruits[1] = "orange"
+print(fruits)
+
+colors = ["red", "green", "blue"]
+colors.append("yellow")
+print(colors)
+
+animals = ["cat", "dog", "rabbit"]
+animals.insert(1,"lion")
+print(animals)
+
+cities = ["Delhi", "Mumbai", "Chennai", "Bangalore"]
+cities.remove("Mumbai")
+print(cities)
+
+cars = ["BMW", "Audi", "Tesla", "Toyota"]
+removed = cars.pop()
+print(removed)
+print(cars)
+
+
+students = ["John", "Sara", "Mike", "Emma", "David"]
+print(len(students))
+
+fruits = ["apple", "banana", "cherry"]
+
+if fruits == "banana":
+    print("Found")
+else:
+    print("Not found")
+
+fruits = ["Apple", "Banana", "Cherry"]
+
+if "Cherry" in fruits:
+    print("Found")
+else:
+    print("Not found")
+
--- Python List, set, Dictonary/Dictionary/List.py
+++
@@ -1,3 +0,0 @@
-mylist = ['apple', 'banana', 'cherry']
-mylist[1:2] = ['kiwi', 'mango']
-print(mylist[2])

791dc512223af5b3ede22b8165b05aa7d63c59f0
Nandhakumar <Nandhaselvi45@gmail.com>
2026-08-11 00:07:24 +0530

    Class and object

--- Python Match case/python match case.py
+++ Python Match case/python match case.py
@@ -41,3 +41,110 @@ match operator:
     case "/":
         print(a/b)
 
+choice = 2
+match choice:
+    case 1:
+        print("Add")
+    case 2:
+        print("View")
+    case 3:
+        print("update")
+    case 4:
+        print("Delete")
+    case _:
+        print("Invalid choice")
+
+month = 4
+
+match month:
+    if 1 month 12:
+        case 1:
+            print("January")
+        case 2:
+            print("February")
+        case 3:
+            print("March")
+        case 4:
+            print("April")
+    else:
+        print("Invalid Month")
+
+grade = "B"
+
+match grade:
+    case "A":
+        print("Excellent")
+    case "B":
+        print("Good")
+    case "C":
+        print("Average")
+    case "D":
+        print("Needs improvement")
+    case "F":
+        print("Failed")
+    case _:
+        print("Invalid grade")
+
+option = 3
+match option:
+    case 1:
+        print("Check Balance")
+    case 2:
+        print("Deposit")
+    case 3:
+        print("Withdraw")
+    case 4:
+        print("Exit")
+
+animal = "dog"
+
+match animal:
+    case "dog":
+        print("woof")
+    case "cat":
+        print("Meow")
+    case "cow":
+        print("Moo")
+    case "lion":
+        print("Roar")
+    case _:
+        print("Unknown animal")
+
+day = "Saturday"
+
+match day:
+    case "Saturday" and "Sunday":
+        print("Weekend")
+    case "Monday":
+        print("Weekday")
+    case "Tuesday":
+        print("Weekday")
+    case "Wednesday":
+        print("Weekday")
+    case "Thuresday":
+        print("Weekday")
+    case "Friday":
+        print("Weekday")
+    case _:
+        print("Invalid day")
+
+number = 2
+
+match number:
+    case number = 1 , 2, 3:
+        print("small")
+
+    case number = 4, 5, 6:
+        print("Medium")
+
+    case number = 7, 8, 9, 10
+        print("Large")
+
+    case _:
+        print("Out of range")
+
+
+
+
+
+

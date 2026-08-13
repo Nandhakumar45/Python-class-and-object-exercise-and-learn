275c1d54d823070b9c7e009fd6d3d16eafa4e91b
Nandhakumar <Nandhaselvi45@gmail.com>
2026-08-13 16:21:16 +0530

    Class and object

--- Python Range/Python range.py
+++ Python Range/Python range.py
@@ -1,53 +1,1 @@
-### 1. Print numbers from 0 to 9
-
-for i in range(10):
-    print(i)
-
-### 2. Print numbers from 1 to 10
-for i in range(1, 11):
-    print(i)
-
-### 3. Print even numbers from 0 to 20
-
-
-for i in range(0, 21, 2):
-    print(i)
-
-
-### 4. Print odd numbers from 1 to 19
-for i in range(1, 20, 2):
-    print(i)
-
-### 5. Print numbers from 10 down to 1
-for i in range(10, 0, -1):
-    print(i)
-
-
-### 6. Print multiples of 5 from 5 to 50
-for i in range(5, 51, 5):
-    print(i)
-
-
-### 7. Find the sum of numbers from 1 to 100
-total = 0
-
-for i in range(1, 101):
-    total += i
-
-print(total)
-
-### 8. Print the multiplication table of 7
-for i in range(1, 11):
-    print("7 x", i, "=", 7 * i)
-
-### 9. Print numbers from 100 down to 5, decreasing by 5
-for i in range(100, 0, -5):
-    print(i)
-
-### 10. Print numbers divisible by 3 from 1 to 50
-
-for i in range(3, 51, 3):
-    print(i)
-
-### The key thing to remember
-
+### 1. Print numbers from 0 to 9for i in range(10):    print(i)### 2. Print numbers from 1 to 10for i in range(1, 11):    print(i)### 3. Print even numbers from 0 to 20for i in range(0, 21, 2):    print(i)### 4. Print odd numbers from 1 to 19for i in range(1, 20, 2):    print(i)### 5. Print numbers from 10 down to 1for i in range(10, 0, -1):    print(i)### 6. Print multiples of 5 from 5 to 50for i in range(5, 51, 5):    print(i)### 7. Find the sum of numbers from 1 to 100total = 0for i in range(1, 101):    total += iprint(total)### 8. Print the multiplication table of 7for i in range(1, 11):    print("7 x", i, "=", 7 * i)### 9. Print numbers from 100 down to 5, decreasing by 5for i in range(100, 0, -5):    print(i)### 10. Print numbers divisible by 3 from 1 to 50for i in range(3, 51, 3):    print(i)### The key thing to remember

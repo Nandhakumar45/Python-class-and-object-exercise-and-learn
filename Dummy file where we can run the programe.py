1cc303c1e2468b64cd628475eafbfe245298e8f7
Nandhakumar <Nandhaselvi45@gmail.com>
2026-08-17 22:06:21 +0530

    Class and object

--- Dummy file where we can run the programe.py
+++ Dummy file where we can run the programe.py
@@ -1,4 +1,16 @@
+a = 0
+b = 5
 
+if a and b:
+    print("A")
+elif a or b:
+    print("B")
+elif not a:
+    print("C")
+else:
+    print("D")
 
-for i in range(3, 50, 3):
-    print(i)
+#Let say if the value inside the list is True?
+
+x = []
+print(x)
---
+++ Python if else/Pyhon if else.py
@@ -0,0 +1,21 @@
+a = 50
+b = 10
+if a == b:
+    print("1")
+elif a >b:
+    print("2")
+else:
+    print("3")
+
+
+# Create age variable
+age = 20
+# Write if/elif/else
+if age < 13:
+   print("child")
+elif age < 18:
+   print("Teenager")
+else:
+    print("Adult")
+
+

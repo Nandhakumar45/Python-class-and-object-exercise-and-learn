b92f49ade3909389dc11e99b020a8678367fa58c
Nandhakumar <Nandhaselvi45@gmail.com>
2026-08-27 19:36:00 +0530

    Class and object

--- Python LOOPS/For loops/For loops.py
+++ Python LOOPS/For loops/For loops.py
@@ -1,109 +1,121 @@
-#1
-for i in range(1, 21):
-    print(i)
-#2
-for i in range(20, 1):
-    print(i)
-#3
-for i in range(1, 50):
-    z = i / 2
-    print(z)
-#4
-for i in range(1, 50):
-    z = i / 2 != 0
-    print(z)
-#5
-#Not sure how do this
+# #1
+# for i in range(1, 21):
+#     print(i)
+# #2
+# for i in range(20, 1):
+#     print(i)
+# #3
+# for i in range(1, 50):
+#     z = i / 2
+#     print(z)
+# #4
+# for i in range(1, 50):
+#     z = i / 2 != 0
+#     print(z)
+# #5
+# #Not sure how do this
+#
+# #6
+# fruits = ["apple", "banana", "cherry", "orange"]
+# for i in fruits:
+#     print(i)
+#
+# #7
+# word = "PYTHON"
+# for i in word:
+#     print(i)
+#
+# #8
+# word = "banana"
+# for i in len(word):
+#     print(i)
+#
+# #9
+# word = "programming"
+# x = ["a", "e", "i", "o", "u"]
+# for x in word:
+#     print(len(x))
+#
+# #10
+# #Not sure how to solve this
+#
+# #11
+# #Not sure how to solve this
+#
+# #12
+# for i in range(1, 100, 5):
+#     print(i)
+#
+# #13
+# for i in range(1, 20):
+#     if i == 10:
+#         continue
+#     print(i)
+#
+# #14
+# for i in range(1, 20):
+#     if i == 15:
+#         break
+#     print(i)
+#
+# #15
+# vegetables = ["Pumpkin", "Carrot"]
+# fruits = ["Apple", "Orange", "Cherry"]
+#
+# for x in vegetables:
+#     for y in fruits:
+#         print(x,y)
+#
+# #You have to learn what is the difference between module operator and % operator
+#
+# # have to work on count
+#
+# word = "banana"
+# count = 0
+#
+# for character in word:
+#     count += 1
+#
+# print(count)
+#
+# word = "banana"
+# count = 0
+#
+# for character in word:
+#     count += 1
+#
+# print(count)
+#
+# word = "programming"
+# vowels = "aeiou"
+# count = 0
+#
+# for character in word:
+#     if character in vowels:
+#         count += 1
+#
+# print(count)
+#
+#
+# #Find the largest numbers
+# numbers = [10, 25, 8, 99, 45]
+#
+# largest = numbers[0]
+#
+# for number in numbers:
+#     if number > largest:
+#         largest = number
+#
+# print(largest)
 
-#6
-fruits = ["apple", "banana", "cherry", "orange"]
-for i in fruits:
-    print(i)
 
-#7
-word = "PYTHON"
-for i in word:
-    print(i)
+name = "Nandha"
 
-#8
-word = "banana"
-for i in len(word):
-    print(i)
-
-#9
-word = "programming"
-x = ["a", "e", "i", "o", "u"]
-for x in word:
-    print(len(x))
-
-#10
-#Not sure how to solve this
-
-#11
-#Not sure how to solve this
-
-#12
-for i in range(1, 100, 5):
-    print(i)
-
-#13
-for i in range(1, 20):
-    if i == 10:
-        continue
-    print(i)
-
-#14
-for i in range(1, 20):
-    if i == 15:
-        break
-    print(i)
-
-#15
-vegetables = ["Pumpkin", "Carrot"]
-fruits = ["Apple", "Orange", "Cherry"]
-
-for x in vegetables:
-    for y in fruits:
-        print(x,y)
-
-#You have to learn what is the difference between module operator and % operator
-
-# have to work on count
-
-word = "banana"
-count = 0
-
-for character in word:
-    count += 1
-
-print(count)
-
-word = "banana"
-count = 0
-
-for character in word:
-    count += 1
-
-print(count)
-
-word = "programming"
-vowels = "aeiou"
-count = 0
-
-for character in word:
-    if character in vowels:
-        count += 1
-
-print(count)
+for i in name:
+    print(name)
 
 
-#Find the largest numbers
-numbers = [10, 25, 8, 99, 45]
+word = "Python"
 
-largest = numbers[0]
-
-for number in numbers:
-    if number > largest:
-        largest = number
-
-print(largest)
+for letter in word:
+    print(letter)

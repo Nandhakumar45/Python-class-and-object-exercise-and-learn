bd11fe4dbadf5aa192f18bbf6b0d7c702266fa24
Nandhakumar <Nandhaselvi45@gmail.com>
2026-08-29 18:16:32 +0530

    Class and object

---
+++ Dummy file where we can run the programe.py
@@ -0,0 +1,4 @@
+num = 1
+while num <= 10:
+    print(f"5 * {num} = {5 * num}")
+    num +=1
---
+++ Python LOOPS/While Loops/While loop.py
@@ -0,0 +1,67 @@
+
+#1
+num = 1
+while num <= 10:
+    print(num)
+    num +=1
+
+#2
+num = 10
+while num >= 1:
+    print(num)
+    num -=1
+
+#3
+num = 1
+while num <= 20:
+    if num % 2 == 0:
+        print(num)
+    num +=1
+
+#4
+num = 1
+while num <= 19:
+    if num % 2 != 0:
+        print(num)
+    num +=1
+
+#5
+#Not sure how to solve this
+
+#6
+#Not sure how to solve this
+
+#7
+num = 1
+while num <= 10:
+    print(f"5 X {num} = {5 * num}")
+    num +=1
+#8
+#Not sure how to solve this
+
+#9
+text = "Python"
+while text:
+    print(text)
+    exit()
+
+#10
+text = "Python"
+while text:
+    pass
+#need to start work on this
+# Overall
+# Assessment
+# While
+# loop
+# syntax: 8 / 10
+# Conditions( if, % ): 8 / 10
+# Loop
+# control( += 1, -= 1): 8 / 10
+# Accumulators(total +=): 3 / 10
+# Counters(count +=): 4 / 10
+# String
+# traversal: 2 / 10
+# Reverse
+# string
+# logic: 1 / 10

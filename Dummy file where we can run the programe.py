b8ac69ccce7c80900ae8d0c43740ec9295123c5f
Nandhakumar <Nandhaselvi45@gmail.com>
2026-09-05 12:26:00 +0530

    Class and object

--- Dummy file where we can run the programe.py
+++ Dummy file where we can run the programe.py
@@ -1,4 +1,58 @@
-num = 1
-while num <= 10:
-    print(f"5 * {num} = {5 * num}")
-    num +=1
+# num = 1
+# while num <= 20:
+#     print(num)
+#     num += 1
+#
+# num = 20
+# while num >= 1:
+#     print(num)
+#     num -= 1
+
+# num = 1
+# while num <= 20:
+#     if num % 2 == 0:
+#         print(num)
+#     num += 1
+
+# num = 1
+# while num <= 10:
+#     print(f"7 * {num} = {7 * num}")
+#     num += 1
+
+#numbers = [10, 20, 30, 40, 50]
+
+# count = 0
+# for i in range(1, 6):
+#     count += i
+# print(count)
+#
+# count = 0
+# for i in range(1, 10):
+#     count += i
+# print(count)
+
+# total = 0
+#
+# for i in range(2, 11, 2):
+#     total += i
+#
+# print(total)
+
+# total = 0
+# for i in range(1, 10):
+#     if i % 2 != 0:
+#         total += i
+# print(total)
+#
+# total = 0
+# for i in range(1, 11):
+#     if i % 2 == 0:
+#         total += i
+# print(total)
+
+count = 0
+numbers = [5, 10, 15]
+
+for i in numbers:
+    count += i
+print(count)

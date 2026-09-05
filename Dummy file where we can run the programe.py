be23b78d04e6ca3e8f2081f7493ef641c1d40a7d
Nandhakumar <Nandhaselvi45@gmail.com>
2026-09-05 17:51:42 +0530

    Class and object

--- Dummy file where we can run the programe.py
+++ Dummy file where we can run the programe.py
@@ -1,18 +1,2 @@
-while True:
-    try:
-        pin = int(input("Enter your PIN: "))
-
-        if pin == 1234:
-            print("PIN is correct")
-        else:
-            raise ValueError("Incorrect PIN")
-
-    except ValueError:
-        print("Invalid PIN. Please try again.")
-
-    else:
-        print("Welcome to your account")
-        break
-
-    finally:
-        print("ATM session checked")
+x = int("10")
+print(x)
--- Python Try-Expect-Else-Finally/Exercies solve.py
+++ Python Try-Expect-Else-Finally/Exercies solve.py
@@ -20,3 +20,89 @@ try:
     print(f"Value at index {index}: {numbers[index]}")
 except IndexError:
     print("Invalid index")
+
+#4
+try:
+    first_number = int(input("Enter the first number"))
+    second_number = int(input("Enter the second number"))
+    result = first_number / second_number
+    print(result)
+
+except ZeroDivisionError:
+    print("Invalid number")
+except ValueError:
+    print("Invalid number")
+except TypeError:
+    print("Invalid number")
+
+#5
+student = {
+    "name": "John",
+    "age": 25,
+    "city": "Chennai"
+}
+
+try:
+    key = input("Enter key: ")
+    print(student[key])
+
+except KeyError:
+    print("Key not found in dictionary")
+
+#6
+try:
+    number = int (input("Enter a number"))
+    num    = 100 / number
+except (ValueError, ZeroDivisionError):
+    print("Enter the correct value")
+
+#7
+balance = 10000
+
+try:
+    withdraw = int(input("How much Rupees do you want to withdraw? "))
+
+    if withdraw <= 0:
+        print("Please enter a valid amount")
+
+    elif withdraw > balance:
+        print("Insufficient balance")
+
+    else:
+        print("Withdrawal successful")
+
+except ValueError:
+    print("Please enter a valid number")
+
+#8
+#My answer would be ZeroDivisionError
+try:
+    x = int("10")
+    y = 0
+    result = x / y
+    print(result)
+
+except ValueError:
+    print("Value Error")
+
+except ZeroDivisionError:
+    print("Zero Division Error")
+
+print("Finished")
+
+#9
+try:
+    number = int(input("Enter number: "))
+    print(100 / number)
+
+except ValueError:
+    #Type of error not defined
+    print("Error")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+
+
+
+
+

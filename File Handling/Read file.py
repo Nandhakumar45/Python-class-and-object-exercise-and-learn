b4d7a53995b7a839e03424c65dd8b4c04cfeb61c
Nandhakumar <Nandhaselvi45@gmail.com>
2026-09-12 16:34:11 +0530

    Class and object

---
+++ File Handling/Read file.py
@@ -0,0 +1,23 @@
+#1
+file = open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\Hello.txt", "r")
+content = file.read()
+print(content)
+file.close()
+
+#2
+file = open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\Hello2.txt", "r")
+content = file.read()
+print(content)
+file.close()
+
+#3
+file = open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\Hello2.txt", "r")
+content = file.read()
+print(content)
+file.close()
+
+#4
+file = open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\Hello2.txt", "r")
+content = file.read()
+print(content)
+file.close()

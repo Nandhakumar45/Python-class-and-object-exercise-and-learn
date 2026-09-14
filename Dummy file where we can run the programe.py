b690005ba0e8d710ca5c4daa448736da6e32dafa
Nandhakumar <Nandhaselvi45@gmail.com>
2026-09-14 15:34:21 +0530

    Class and object

--- Dummy file where we can run the programe.py
+++ Dummy file where we can run the programe.py
@@ -1,9 +1,4 @@
-try:
-    x = 10 / 2
-    print("Try successful")
-
-except ZeroDivisionError:
-    print("Error")
-
-finally:
-    print("Finally")
+file = open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\Hello2.txt", "r")
+content = file.readline()
+print(content)
+file.close()
--- File Handling/Read file.py
+++ File Handling/Read file.py
@@ -5,20 +5,43 @@ print(content)
 file.close()
 
 #2
+#This program state that how to print the first line in the programme
 file = open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\Hello2.txt", "r")
-content = file.read()
+content = file.readline()
 print(content)
 file.close()
+#To print the second line follow this step uisng the index of the programming
+file = open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\Hello2.txt", "r")
+
+lines = file.readlines()
+
+print(lines[1])
+
+file.close()
+
 
 #3
 file = open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\Hello2.txt", "r")
-content = file.read()
+content = file.readlines()
 print(content)
 file.close()
+
 
 #4
 file = open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\Hello2.txt", "r")
 content = file.read()
-print(content)
+print(len(content))
 file.close()
+
+#5
+file = open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\Hello3.txt", "r")
+content = file.read()
+
+if "Python" in content:
+    print("Word exists in the file")
+else:
+    print("Word not found")
+
+file.close()
+
 

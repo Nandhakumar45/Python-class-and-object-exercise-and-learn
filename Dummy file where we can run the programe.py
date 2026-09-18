996f20e8fbe55589c94a7d0ca7e0c027ff381d39
Nandhakumar <Nandhaselvi45@gmail.com>
2026-09-18 20:14:05 +0530

    Class and object

--- Dummy file where we can run the programe.py
+++ Dummy file where we can run the programe.py
@@ -1,4 +1,8 @@
-for x in range(1,6):
-    name = input("Enter your name:")
+while True:
+    name = input("Enter numbers:")
     with open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\append mode.txt", "a") as file:
         file.write(f"{name}\n")
+
+    if name == "exit":
+        print("I exit from the condition")
+        break
--- Python File Handling/Append/Append.py
+++ Python File Handling/Append/Append.py
@@ -11,12 +11,34 @@ with open (r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\ap
 name = input("Enter your name")
 with open (r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\append mode.txt", "a") as file:
     file.write(name)
-
+# #This is the best way to write a code when compared to previous one. add + \n in last line
+# name = input("Enter your name: ")
+#
+# with open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\append mode.txt", "a") as file:
+#     file.write(name + "\n")
 #4
 for x in range(1,6):
     name = input("Enter your name:")
     with open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\append mode.txt", "a") as file:
         file.write(f"{name}\n")
+#5
+for x in range(1,6):
+    numbers = input("Enter numbers:")
+    with open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\append mode.txt", "a") as file:
+        file.write(f"{numbers}\n")
 
+# #This is the best way to write a code when compared to previous one. add + \n in last line
+# name = input("Enter your name: ")
+#
+# with open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\append mode.txt", "a") as file:
+#     file.write(name + "\n")
 
+#6
+while True:
+    name = input("Enter numbers:")
+    with open(r"C:\Users\320287287\OneDrive - Philips\Desktop\File Handling\Read\append mode.txt", "a") as file:
+        file.write(f"{name}\n")
 
+    if name == "exit":
+        print("I exit from the condition")
+        break
